0ec849711d1cf8f3c7aa4682d83e710e739101d4
Joe Tester <joe@tester.com>
2016-03-16 15:39:33 -0400

    Editing this file that will cause the conflict that you would need to resolve...

--- first_github_pratice.php
+++ first_github_pratice.php
@@ -1,6 +1,6 @@
 <?php
 
-	echo "This is first editing of github_practice that will push over to main repository on github..." ;
-
+	#echo "This is first editing of github_practice that will push over to main repository on github..." ;
+	echo "Write something in this file that will cause conflict..." ;
 
 ?>
