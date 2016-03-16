fc418c1730a31563746269de67554ee96da40ffb
Chester Tester <chester@tester.com>
2016-03-16 15:36:24 -0400

    Second editing on first_github_practice.php file...

--- first_github_pratice.php
+++ first_github_pratice.php
@@ -1,6 +1,6 @@
 <?php
 
-	echo "This is first editing of github_practice that will push over to main repository on github..." ;
-
+	#echo "This is first editing of github_practice that will push over to main repository on github..." ;
+	echo "Second editing of this file..." ;
 
 ?>
