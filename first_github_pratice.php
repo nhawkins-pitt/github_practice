3c5d2254b31889755eac2d3c3a8e0f3af12be759
Chester Tester <chester@tester.com>
2016-03-17 13:47:44 -0400

    Made another edit of this file which also was edited in other branch to see what GIT will do with it...

--- first_github_pratice.php
+++ first_github_pratice.php
@@ -1,6 +1,7 @@
 <?php
 
 	#echo "This is first editing of github_practice that will push over to main repository on github..." ;
-	echo "Write something in this file that will cause conflict and now it is being resolved at 3:43 p.m." ;
+	#echo "Write something in this file that will cause conflict and now it is being resolved at 3:43 p.m." ;
+	echo "Another editing on this same file which was also edited in other branch to see what GIT will do with it..." ;
 
 ?>
