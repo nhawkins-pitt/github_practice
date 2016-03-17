e62e40a5e9f4998c2776d1ebe59ccd0333251be4
Chester Tester <chester@tester.com>
2016-03-17 15:55:54 -0400

    Testing with git rebase command...

--- fifth_github_practice.php
+++ fifth_github_practice.php
@@ -1,5 +1,6 @@
 <?php
 
 	#echo "This is fifth file to be editing and push into new branch remotely..." ;
-	echo "Another test to push this file directly into origin/newBranch remotely..." ;
+	#echo "Another test to push this file directly into origin/newBranch remotely..." ;
+	echo "Testing with git rebase command..." ;
 ?>
