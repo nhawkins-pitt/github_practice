388f76a6cf2f39c839eb00327b334c06b2c2c059
Chester Tester <chester@tester.com>
2016-03-17 14:27:48 -0400

    Fix the bug and then merge into master and attempting to use mergetool and understand how to use it...

--- third_github_practice.php
+++ third_github_practice.php
@@ -1,5 +1,5 @@
 <?php
 
-	echo "This is third editing of github_practice file to test..." ;
-
+	#echo "This is third editing of github_practice file to test..." ;
+	echo "Fix the bug and then merge into master and attempting to use mergetool to understand how to use it..." ;
 ?>
