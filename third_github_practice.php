36b6d793e5cbdff162c8c59921a71e530b68e5ea
Chester Tester <chester@tester.com>
2016-03-17 14:31:29 -0400

    Finished up writing statement about mergetool on this file...

--- third_github_practice.php
+++ third_github_practice.php
@@ -1,6 +1,6 @@
 <?php
 
 	#echo "This is third editing of github_practice file to test..." ;
-	echo "Testing with git mergetool option to merge file(s) into another branch..." ;
+	echo "Testing with git mergetool option to merge file(s) into another branch...." ;
 
 ?>
