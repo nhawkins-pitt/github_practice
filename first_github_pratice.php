2413be2a6a7de0010ee065cdecc4abec86144fcf
Chester Tester <chester@tester.com>
2016-03-17 13:43:19 -0400

    Editing this file as well as work in other branch on same file to see the result...

--- first_github_pratice.php
+++ first_github_pratice.php
@@ -1,6 +1,7 @@
 <?php
 
 	#echo "This is first editing of github_practice that will push over to main repository on github..." ;
-	echo "Write something in this file that will cause conflict and now it is being resolved at 3:43 p.m." ;
-
+	#echo "Write something in this file that will cause conflict and now it is being resolved at 3:43 p.m." ;
+	echo "Editing this file on current branch as well as switch over to another branch and do the same work to see the result..." ;
+	
 ?>
