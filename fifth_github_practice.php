a60718c1298a1564139ba3aaa0fed48d500de905
Joe Tester <joe@tester.com>
2016-03-18 13:26:48 -0400

    Deleted this file, fifth_github_practice.php

--- fifth_github_practice.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	#echo "This is fifth file to be editing and push into new branch remotely..." ;
-	#echo "Another test to push this file directly into origin/newBranch remotely..." ;
-	echo "Testing with git rebase command..." ;
-?>
